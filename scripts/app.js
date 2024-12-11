e3293be68e6cabeaa237b9d9912e5744dbcf3a00
MandoxaElemental <gprcards@gmail.com>
2024-12-11 12:06:47 -0800

    just testing something out

--- scripts/app.js
+++ scripts/app.js
@@ -19,19 +19,24 @@ let humidity = document.getElementById('humidity');
 let day1h = document.getElementById('day1h');
 let day1l = document.getElementById('day1l');
 let weatherIcon1 = document.getElementById('weatherIcon1');
+let day1weather = "";
 let day2h = document.getElementById('day2h');
 let day2l = document.getElementById('day2l');
 let weatherIcon2 = document.getElementById('weatherIcon2');
+let day2weather = "";
 let day3h = document.getElementById('day3h');
 let day3l = document.getElementById('day3l');
 let weatherIcon3 = document.getElementById('weatherIcon3');
+let day3weather = "";
 let day4h = document.getElementById('day4h');
 let day4l = document.getElementById('day4l');
 let weatherIcon4 = document.getElementById('weatherIcon4');
+let day4weather = "";
 let day5h = document.getElementById('day5h');
 let day5l = document.getElementById('day5l');
 let weatherIcon5 = document.getElementById('weatherIcon5');
-let cityName = "London"
+let day5weather = "";
+let cityName = "Stockton"
 let savedArr = [];
 let recentArr = [];
 
@@ -105,14 +110,19 @@ async function apiCall(){
        humidity.innerText = data.list[0].main.humidity + "%";
        day1h.innerText = "H: " + Math.round((data.list[0].main.temp_max * 9/5) - 459.67) + "°F";
        day1l.innerText = "L: " + Math.round((data.list[0].main.temp_min * 9/5) - 459.67) + "°F";
+       day1weather = data.list[0].weather[0].main;
        day2h.innerText = "H: " + Math.round((data.list[1].main.temp_max * 9/5) - 459.67) + "°F";
        day2l.innerText = "L: " + Math.round((data.list[1].main.temp_min * 9/5) - 459.67) + "°F";
+       day2weather = data.list[1].weather[0].main;
        day3h.innerText = "H: " + Math.round((data.list[2].main.temp_max * 9/5) - 459.67) + "°F";
        day3l.innerText = "L: " + Math.round((data.list[2].main.temp_min * 9/5) - 459.67) + "°F";
+       day3weather = data.list[2].weather[0].main;
        day4h.innerText = "H: " + Math.round((data.list[3].main.temp_max * 9/5) - 459.67) + "°F";
        day4l.innerText = "L: " + Math.round((data.list[3].main.temp_min * 9/5) - 459.67) + "°F";
+       day4weather = data.list[3].weather[0].main;
        day5h.innerText = "H: " + Math.round((data.list[4].main.temp_max * 9/5) - 459.67) + "°F";
        day5l.innerText = "L: " + Math.round((data.list[4].main.temp_min * 9/5) - 459.67) + "°F";
+       day5weather = data.list[4].weather[0].main;
 
        fahrenheit.addEventListener('click', function(){
         fahrenheit.className = 'active';
@@ -150,15 +160,71 @@ async function apiCall(){
                day5l.innerText = "L: " + Math.round((Math.round((data.list[4].main.temp_min * 9/5) - 459.67) - 32) / (9/5)) + "°C";
     });
 
-    if(weather.innerText.toLowerCase() == "rain"){
+    if(weather.innerText == "Rain"){
         document.getElementById('currentWeatherImg').src="../assets/rainy.png"
-    }else if(weather.innerText.toLowerCase() == "clear"){
+    }else if(weather.innerText == "Clear"){
         document.getElementById('currentWeatherImg').src="../assets/sunny.png"
-    }else if(weather.innerText.toLowerCase() == "clouds"){
+    }else if(weather.innerText == "Clouds"){
         document.getElementById('currentWeatherImg').src="../assets/cloud.png"
-    }else if(weather.innerText.toLowerCase() == "clouds"){
-        document.getElementById('currentWeatherImg').src="../assets/cloud.png"
+    }else if(weather.innerText == "Snow"){
+        document.getElementById('currentWeatherImg').src="../assets/snow.png"
+    };
+    
+    if(day1weather == "Rain"){
+        document.getElementById('weatherIcon1').src="../assets/rainy.png"
+    }else if(day1weather == "Clear"){
+        document.getElementById('weatherIcon1').src="../assets/sunny.png"
+    }else if(day1weather == "Clouds"){
+        document.getElementById('weatherIcon1').src="../assets/cloud.png"
+    }else if(day1weather == "Snow"){
+        document.getElementById('weatherIcon1').src="../assets/snow.png"
+    };
+
+    if(day2weather == "Rain"){
+        document.getElementById('weatherIcon2').src="../assets/rainy.png"
+    }else if(day2weather == "Clear"){
+        document.getElementById('weatherIcon2').src="../assets/sunny.png"
+    }else if(day2weather == "Clouds"){
+        document.getElementById('weatherIcon2').src="../assets/cloud.png"
+    }else if(day2weather == "Snow"){
+        document.getElementById('weatherIcon2').src="../assets/snow.png"
+    };
+
+    if(day3weather == "Rain"){
+        document.getElementById('weatherIcon3').src="../assets/rainy.png"
+    }else if(day3weather == "Clear"){
+        document.getElementById('weatherIcon3').src="../assets/sunny.png"
+    }else if(day3weather == "Clouds"){
+        document.getElementById('weatherIcon3').src="../assets/cloud.png"
+    }else if(day3weather == "Snow"){
+        document.getElementById('weatherIcon3').src="../assets/snow.png"
+    };
+
+    if(day4weather == "Rain"){
+        document.getElementById('weatherIcon4').src="../assets/rainy.png"
+    }else if(day4weather == "Clear"){
+        document.getElementById('weatherIcon4').src="../assets/sunny.png"
+    }else if(day4weather == "Clouds"){
+        document.getElementById('weatherIcon4').src="../assets/cloud.png"
+    }else if(day4weather == "Snow"){
+        document.getElementById('weatherIcon4').src="../assets/snow.png"
+    };
+
+    if(day5weather == "Rain"){
+        document.getElementById('weatherIcon5').src="../assets/rainy.png"
+    }else if(day5weather == "Clear"){
+        document.getElementById('weatherIcon5').src="../assets/sunny.png"
+    }else if(day5weather == "Clouds"){
+        document.getElementById('weatherIcon5').src="../assets/cloud.png"
+    }else if(day5weather == "Snow"){
+        document.getElementById('weatherIcon5').src="../assets/snow.png"
     }
+
+    console.log(day1weather)
+    console.log(day2weather)
+    console.log(day3weather)
+    console.log(day4weather)
+    console.log(day5weather)
 };
     
     apiCall();
--- scripts/environments.js
+++
@@ -1,5 +0,0 @@
-
-const APIKEY = "24a9b050efef77706f77d5c8bba1e81d";
-const fiveDayKey = "bc9fbb7eaadc368de3ca8a6e14057080";
-
-export { APIKEY, fiveDayKey } 

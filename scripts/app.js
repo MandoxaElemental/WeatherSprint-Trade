593bdf2910522af7909c8712bab7755bce5dcd5f
MandoxaElemental <gprcards@gmail.com>
2024-12-12 15:30:59 -0800

    Just finished with the ability to pull in the info from previously searched inputs

--- scripts/app.js
+++ scripts/app.js
@@ -45,6 +45,7 @@ let fetchLink = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&
 let recentList = false;
 let saveList = false;
 
+
 //Geo location is a built in API that allows the user to share there location apon request.
 
 //navigator.geolocation this return geolocation object
@@ -202,7 +203,7 @@ async function apiCall(){
         document.getElementById('weatherIcon5').src="../assets/snow.png"
     }
 };
-    
+
     apiCall();
 
 console.log(cityName);
@@ -222,19 +223,32 @@ search.addEventListener('keypress', (e) => {
         fetchLink = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKEY}`;
         
         recentArr.push(cityName);
-            if(recentArr.length > 5){
-                recentArr.shift();
-            }
-                let listItem = document.createElement("a");
-                listItem.setAttribute("id", "test");
-                listItem.textContent = cityName;
-                let removeBtn = document.createElement('button');
-                removeBtn.type = 'button';
-                removeBtn.className = "btn btn-danger btn-sm";
-                removeBtn.innerText = "Delete Name";
-                myDropdown.appendChild(listItem);
-                listItem.appendChild(removeBtn);
+
+        myDropdown.innerHTML = "";
+        
+        if(recentArr.length > 5){
+            recentArr.shift();
+        }
+            
+        for(let i=0; i < recentArr.length; i++){
+            let listItem = document.createElement("p");
+            listItem.setAttribute("id", 'city_' + i);
+            listItem.addEventListener('click', function(e) {
+                cityName = listItem.textContent;
+                fetchLink = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${APIKEY}`;
+                apiCall();
+            });
+            listItem.textContent = recentArr[i];
+            let saveBtn = document.createElement('button');
+            saveBtn.type = 'button';
+            saveBtn.className = "saveBtn";
+            saveBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+</svg>`;
+            myDropdown.appendChild(listItem);
+            listItem.appendChild(saveBtn);
             console.log(recentArr)
+        }
         apiCall();
-    }
-})
+    };
+});

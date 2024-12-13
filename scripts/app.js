8de7ded3aab979f25335c627734f265ae23930ba
MandoxaElemental <gprcards@gmail.com>
2024-12-13 15:48:09 -0800

    almost finished

--- scripts/app.js
+++ scripts/app.js
@@ -268,11 +268,9 @@ search.addEventListener('click', function(e){
 saved.addEventListener('click', function(e){
     document.getElementById("savedDropdown").classList.toggle("show");
     createElements();
-
     function createElements(){
         let testStorage = getFromLocalStorage();
         testStorage.map(SavedCountries => {
-            console.log(SavedCountries);
             
             let p = document.createElement('p');
             p.innerText = SavedCountries;
@@ -286,6 +284,7 @@ saved.addEventListener('click', function(e){
             removeBtn.addEventListener('click', function(){
                 removeFromLocalStorage(SavedCountries);
                 p.remove();
+                alert(`Removed ${p.innerText} from your Saved`)
             });
     
             p.appendChild(removeBtn);
@@ -310,7 +309,7 @@ search.addEventListener('keypress', (e) => {
         }
             
         for(let i=0; i < recentArr.length; i++){
-            let saveBool = false
+            let saveBool = true
             let listItem = document.createElement("p");
             listItem.setAttribute("id", 'city_' + i);
             listItem.innerHTML = `<span id='entry_${i}' class='entry'>${recentArr[i]}</span>`;
@@ -318,30 +317,32 @@ search.addEventListener('keypress', (e) => {
             saveBtn.type = 'button';
             saveBtn.className = "saveBtn";
             for(let j = 0; j < recentArr.length; j++){
-                if(search.value != recentArr[j]){
-                    saveBtn.innerHTML = `<svg id='saved_${i}' class='starBtn' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-                    </svg>`;
-                }else{
+                if(getFromLocalStorage().includes(recentArr[j])){
                     saveBtn.innerHTML = `<svg id='saved_${i}' class='starBtn' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="yellow" class="bi bi-star-fill" viewBox="0 0 16 16">
                     <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                     </svg>`
+                }else{
+                    saveBtn.innerHTML = `<svg id='saved_${i}' class='starBtn' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                    <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
+                    </svg>`;
                 }
             }
             saveBtn.addEventListener('click', function(e) {
-                if(!saveBool){
-                    saveBtn.innerHTML = `<svg id='saved_${i}' class='starBtn' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="yellow" class="bi bi-star-fill" viewBox="0 0 16 16">
-                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-                    </svg>`
-                    saveBool = true;
-                    saveToLocalStorage(recentArr[i]);
-
-                } else {
+                if(getFromLocalStorage().includes(recentArr[i])){
                     saveBtn.innerHTML = `<svg id='saved_${i}' class='starBtn' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                     <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
                     </svg>`;
-                    saveBool = false;
+
                     removeFromLocalStorage(recentArr[i]);
+                    alert(`Removed ${recentArr[i]} from your Saved`)
+                    
+                } else {
+                    saveBtn.innerHTML = `<svg id='saved_${i}' class='starBtn' xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="yellow" class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+                    </svg>`
+                    saveBool = false;
+                    saveToLocalStorage(recentArr[i]);
+                    alert(`Added ${recentArr[i]} to your Saved`);
                 }
             });
             myDropdown.appendChild(listItem);
